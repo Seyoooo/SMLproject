9d462ba63cb41a4224b91899525bdb160b4941e4
Seyoooo <51091250+Seyoooo@users.noreply.github.com>
2024-01-06 12:42:53 +0100

    Update movies-filling-database.py

--- movies-filling-database.py
+++ movies-filling-database.py
@@ -8,8 +8,8 @@ def g():
     nb_pages = 20
     first_page = 0
 
-    lte_release_date = '2015-12-31'
-    gte_release_date = '2015-01-01'
+    lte_release_date = '2012-12-31'
+    gte_release_date = '2012-01-01'
 
     print(f'Fetching {nb_pages} pages beggining at page {first_page}, form {gte_release_date} to {lte_release_date}.')
     print('------')
